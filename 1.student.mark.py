db046022ac7c643a6b545ecfc03a7d8cafbeb598
Tonguesten36 <tung@tungsoft.net>
2023-02-16 15:06:58 +0700

    Divide input_func() and listing_func() into smaller functions

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,129 +1,53 @@
 
-# Student mark management system, with tuples, dicts, lists
-# Functions include:
-#   Input function
-#       • Input a number of students in a class (a list of student dictionaries) (done)
-#       • Input student info: id, name, dob (dictionary)
-#       • Input number of courses (a list of courses dictionaries)
-#       • Input course info: id, name (dictionary)
-#       • Select a course, input mark for students in this course
-#   Listing functions:
-#       • List courses
-#       • List students
-#       • Show student marks for a given course
-
-# The input function
-def input_func():
-    # Number of students
-    no_students = 0
-    # Number of courses
-    no_courses = 0
-
-    # Lists of students
-    student_list = []
-    # Lists of courses 
-    course_list = []
-    # Lists of students' marks in courses
-    student_mark_list = []
-
-    # Input number of students in the class, force the user to input again if the input is invalid
+# Input a number of students in an class (done)
+def input_student(students_list):
+    # Ask the user how many students would they like to add
     while True:
         try:
-            no_students = int(input("Input number of students in the class: "))
+            no_students = int(input("Input number of students to be added in the class: "))
             if no_students <= 0:
                 print("There must be at least one student")
             else:
                 break
         except:
             print("Must be an integer!")
-    
-    # Input number of courses, force the user to input again if the input is invalid
-    while True:
-        try:
-            no_courses = int(input("Input number of courses: "))
-            if no_courses <= 0:
-                print("There must be at least one course")
-            else:
-                break
-        except:
-            print("Must be an integer!")
-    
-    # Input student info: id, name, dob (dictionary)
-    print("-----Students-----")
+
+    # Inputting neccessary info for students
     for s in range(0, no_students, 1):
         while True:
             try:
-                student = {}
+                student_dict = {}
+                print(f"---Student {s}. ")
                 student_id = int(input("Student ID: "))
                 student_name = str(input("Name: "))
                 student_dob = str(input("D.O.B (DD/MM/YYYY): "))
+
                 if student_id <= 0 or student_name == "" or student_dob == "":
                     print("try again")
                 else:
-                    student.update({"id":student_id})
-                    student.update({"name":student_name})
-                    student.update({"dob":student_dob})
+                    # Check if the student exist yet
+                    for student in students_list:
+                        name = student.get("name")
+                        _id = student.get("id")
+                        if student_name == name or student_id == _id:
+                            print("Student already exist!")
+                            return
 
-                    student_list.append(student)
+                    student_dict.update({"id":student_id})
+                    student_dict.update({"name":student_name})
+                    student_dict.update({"dob":student_dob})
+
+                    students_list.append(student_dict)
                     break
             except:
                 print("Something went wrong, try again.")
 
-    # Input course info: course name, id
-    print("-----Courses-----")
-    for c in range(0, no_courses, 1):
-        while True:
-            try:
-                course = {}
-                course_name = str(input("Course Name: "))
-                course_id = int(input("Course ID: "))
-                if course_id <= 0 or course_name == "":
-                    print("try again")
-                else:
-                    course.update({"name":course_name})
-                    course.update({"id":course_id})
-
-                    course_list.append(course)
-                    break
-            except:
-                print("Something went wrong, try again.")
-
-    # Select a student, input marks for that student in every courses
-    print("-----Marks-----")
-    for student_dict in student_list:
-        for course_dict in course_list:
-            # Create a new, empty dict
-            mark_for_each_course = {}
-            while True:
-                try:
-                    # Ask the user for the student's mark in a course
-                    # Had to put student.get("name") and course.get("name") in separate variables
-                    # in order to avoid SyntaxError
-                    name_student = student_dict.get("name")
-                    name_course = course_dict.get("name")
-                    mark = float(input(f"Mark for student {name_student} in {name_course} is: "))
-                    
-                    # This line of code will return a SyntaxError because of the string "name"
-                    # mark = int(input(f"Mark for student {student.get("name")} in {course.get("name")} is: "))
-
-                    # Update the mark_for_each_course dict 
-                    mark_for_each_course.update({"student_name":student["name"]})
-                    mark_for_each_course.update({"course_name":course["name"]})
-                    mark_for_each_course.update({"mark":mark})
-
-                    student_mark_list.append(mark_for_each_course)
-                    break
-                except:
-                    print("Something went wrong, try again.")
-    # Return a list of students, a list of courses available, and a list of students' marks in courses
-    return student_list, course_list, student_mark_list
-
-# Display list of students, list of courses, and list of students' marks in the courses
-def listing_func(students_list, courses_list, marks_list):
-    # List out the students
-    counter = 1
+# List out all students in the class (Done)
+def list_student(students_list):
     print("---List of students in the class---")
+    counter = 1
     for student_dict in students_list:
+        # Only print out the student's info if all keys exists
         if "name" and "id" and "dob" in student_dict:
             student_name = student_dict.get("name")
             student_id = student_dict.get("id")
@@ -131,8 +55,49 @@ def listing_func(students_list, courses_list, marks_list):
 
             print(f"Student {counter}. Name: {student_name}, ID: {student_id}, D.O.B: {student_dob}")
             counter = counter + 1
-    
-    # List out the courses 
+
+# Input a number of courses (Done)
+def input_course(course_list):
+    # Ask the user how many courses they would like to add
+    while True:
+        try:
+            no_courses = int(input("Input number of courses to be added: "))
+            if no_courses <= 0:
+                print("There must be at least one course")
+            else:
+                break
+        except:
+            print("Must be an integer!")
+
+    # Inputting neccessary info for the courses
+    for c in range(0, no_courses, 1):
+        while True:
+            try:
+                course_dict = {}
+                print(f"---Course {c}. ")
+                course_name = str(input("Course Name: "))
+                course_id = int(input("Course ID: "))
+                if course_id <= 0 or course_name == "":
+                    print("try again")
+                else:
+                    # Check if the course exist yet
+                    for course in course_list:
+                        name = course.get("name")
+                        _id = course.get("id")
+                        if course_name == name | course_id == _id:
+                            print("Course already exist!")
+                            return
+
+                    course_dict.update({"name":course_name})
+                    course_dict.update({"id":course_id})
+
+                    course_list.append(course_dict)
+                    break
+            except:
+                print("Something went wrong, try again.")
+
+# List out available courses (Done)
+def list_course(courses_list):
     counter = 1
     print("---List of courses available---")
     for course_dict in courses_list:
@@ -143,9 +108,123 @@ def listing_func(students_list, courses_list, marks_list):
             print(f"Course {counter}. Name: {course_name}, ID: {course_id}")
             counter = counter + 1
 
+# Select a course, input marks for a student in that courses
+def input_mark(students_list, courses_list, marks_list):
+    student_name = input("Student's name: ")
+
+    # Check if the student exist 
+    if len(students_list) == 0:
+        print("Student does not exist!")
+        return
+    else:
+        for student_dict in students_list:
+            name = student_dict.get("name")
+            if student_name != name:
+                print("Student does not exist!")
+                return
+    
+    course_name = input("Course's name: ")
+
+    # Check if the course exist
+    if len(courses_list) == 0:
+        print("Course does not exist!")
+        return
+    for course_dict in courses_list:
+        name = course_dict.get("name")
+        if course_name != name or len(courses_list) == 0:
+            print("Courses does not exist!")
+            return
+    
+    # Ask the user the student's mark for that course
+    student_mark = 0
+    while True:
+        try:
+            student_mark = float(input("Input mark here: "))
+            if student_mark < 0:
+                print("Mark must be non-negative")
+            else:
+                break
+        except:
+            print("Must be a number!")
+    
+    # The student mark will be a dictionary
+    student_mark_dict = {}
+    student_mark_dict.update({"student_name":student_name})
+    student_mark_dict.update({"course_name":course_name})
+    student_mark_dict.update({"student_mark":student_mark})
+
+    # Add the mark's info into the list
+    marks_list.append(student_mark_dict)
+
+# List out all mark of a student
+def list_mark(marks_list):
+    search_target = input("Whose mark do you want to see?: ")
+
+    for mark_dict in marks_list:
+        if "student_name" and "course_name" and "student_mark" in mark_dict:
+            student_name = mark_dict.get("student_name")
+            course_name = mark_dict.get("course_name")
+            student_mark = mark_dict.get("student_mark")
+
+            # Display the mark of a specific student
+            if search_target == student_name:
+                print(f"Student: {student_name}, Course: {course_name}, Mark: {student_mark}")
 
 # Execute the code
-print("---Student Mark management system---")
-(students, courses, marks) = input_func()
-listing_func(students, courses, marks)
+# This code will loop until terminated
+if __name__ == "__main__":
+    print("------Student Mark management system------")
 
+    students_list = []
+    courses_list = []
+    marks_list = []
+
+    while True:
+        print("---Here are the things that you could do---")
+        print("1. Add new student(s)")
+        print("2. Add new course(s)")
+        print("3. Select a course and input mark for student")
+        print("4. List out all students")
+        print("5. List out all courses")
+        print("6. List out all students' mark for each courses")
+        print("7. Exit")
+        
+        op = input("What do you want to do? (1-7): ")
+
+        # Use this code if you're using a Python version 3.10 or later
+        match op:
+            case "1":
+                input_student(students_list)
+            case "2":
+                input_course(courses_list)
+            case "3":
+                input_mark(students_list, courses_list, marks_list)
+            case "4":
+                list_student(students_list)
+            case "5":
+                list_course(courses_list)
+            case "6":
+                list_mark(marks_list)
+            case "7":
+                exit()
+            case _:
+                print("Invalid input")
+        
+        # Use this code if you're using a Python version earlier than 3.10
+        # if op == "1":
+        #     input_student(students_list)
+        # elif op == "2":
+        #     input_course(courses_list)
+        # elif op == "3":
+        #     input_mark(students_list, courses_list, marks_list)
+        # elif op == "4":
+        #     list_student(students_list)
+        # elif op == "5":
+        #     list_course(courses_list)
+        # elif op == "6":
+        #     list_mark(marks_list)
+        # elif op == "7":
+        #     exit()
+        # else:
+        #     print("Invalid input")
+        print("\n")
