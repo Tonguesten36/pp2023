9516e2d3e155337477807d87d6e8bf8d93e99da7
Tonguesten <tung@tungsoft.net>
2023-03-29 15:45:14 +0700

    Modify some comments

--- pw8/main.py
+++ pw8/main.py
@@ -48,42 +48,80 @@ def main():
         op = input("What do you want to do? (1-9): ")
         
         # Use this code if you're using a Python version 3.10 or later
-        match op:
-            case "1":
-                inp.input_student(students_list)
-            case "2":
-                inp.input_course(courses_list)
-            case "3":
-                inp.input_mark(students_list, courses_list, marks_list)
-            case "4":
-                out.list_student(students_list)
-            case "5":
-                out.list_course(courses_list)
-            case "6":
-                out.list_mark(marks_list)
-            case "7":
-                # Ask the user for the student's ID
-                student_id = str(input("Enter the student ID: "))
-                out.average_gpa(student_id, marks_list, courses_list, True)
-            case "8":
-                # Sort the students by descending GPA scores
-                out.sort_student(students_list, marks_list, courses_list)
-            case "9":    
-                # Serialize all before exit the program
-                serialize_students = threading.Thread(target=comp.serialize_objects, args=(students_list, save_files[0]))
-                serialize_courses = threading.Thread(target=comp.serialize_objects, args=(courses_list, save_files[1]))
-                serialize_marks = threading.Thread(target=comp.serialize_objects, args=(marks_list, save_files[2]))
+        # match op:
+        #     case "1":
+        #         inp.input_student(students_list)
+        #     case "2":
+        #         inp.input_course(courses_list)
+        #     case "3":
+        #         inp.input_mark(students_list, courses_list, marks_list)
+        #     case "4":
+        #         out.list_student(students_list)
+        #     case "5":
+        #         out.list_course(courses_list)
+        #     case "6":
+        #         out.list_mark(marks_list)
+        #     case "7":
+        #         # Ask the user for the student's ID
+        #         student_id = str(input("Enter the student ID: "))
+        #         out.average_gpa(student_id, marks_list, courses_list, True)
+        #     case "8":
+        #         # Sort the students by descending GPA scores
+        #         out.sort_student(students_list, marks_list, courses_list)
+        #     case "9":    
+        #         # Serialize all before exit the program
+        #         serialize_students = threading.Thread(target=comp.serialize_objects, args=(students_list, save_files[0]))
+        #         serialize_courses = threading.Thread(target=comp.serialize_objects, args=(courses_list, save_files[1]))
+        #         serialize_marks = threading.Thread(target=comp.serialize_objects, args=(marks_list, save_files[2]))
 
-                # Start the data serializing process
-                serialize_students.start()
-                serialize_courses.start()
-                serialize_marks.start()
+        #         # Start the data serializing process
+        #         serialize_students.start()
+        #         serialize_courses.start()
+        #         serialize_marks.start()
 
-                # Combine all serialized data into a .tar.gz file before exiting
-                comp.combine_all_to_tarfile(save_files)
-                exit()
-            case _:
-                print("Invalid input")
+        #         # Combine all serialized data into a .tar.gz file before exiting
+        #         comp.combine_all_to_tarfile(save_files)
+        #         exit()
+        #     case _:
+        #         print("Invalid input")
+
+        # Use this code if you're using versions earlier than Python 3.10
+        if op == "1":
+            inp.input_student(students_list)
+        elif op == "2":
+            inp.input_course(courses_list)
+        elif op == "3":
+            inp.input_mark(students_list, courses_list, marks_list)
+        elif op == "4":
+            out.list_student(students_list)
+        elif op == "5":
+            out.list_course(courses_list)
+        elif op == "6":
+            out.list_mark(marks_list)
+        elif op == "7":
+            # Ask the user for the student's ID
+            student_id = str(input("Enter the student ID: "))
+            out.average_gpa(student_id, marks_list, courses_list, True)
+        elif op == "8":
+            # Sort the students by descending GPA scores
+            out.sort_student(students_list, marks_list, courses_list)
+        elif op == "9":
+            # Create threads for serializing all data before exit the program
+            serialize_students = threading.Thread(target=comp.serialize_objects, args=(students_list, save_files[0]))
+            serialize_courses = threading.Thread(target=comp.serialize_objects, args=(courses_list, save_files[1]))
+            serialize_marks = threading.Thread(target=comp.serialize_objects, args=(marks_list, save_files[2]))
+
+            # Start the data serializing process
+            serialize_students.start()
+            serialize_courses.start()
+            serialize_marks.start()
+
+            # Combine all serialized data into a .tar.gz file before exiting
+            comp.combine_all_to_tarfile(save_files)
+            exit()
+        else:
+            print("Invalid input")
+
 
 # Run the code
 main()
